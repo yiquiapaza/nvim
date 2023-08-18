84a17d2df112075863e7eb3b447fdc6fa4d1b76e
Yhonatan Jesus <yiquiapaza@gmail.com>
2023-08-17 22:57:05 -0500

    test: testing prettier cofig with formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,12 +1,20 @@
 return {
 	"mhartington/formatter.nvim",
 	config = function()
+		local util = require("formatter.util")
 		require("formatter").setup({
 			loggin = true,
 			log_level = vim.log.levels.WARN,
 			filetype = {
 				lua = {
 					require("formatter.filetypes.lua").stylua(),
+				},
+				html = {
+					function()
+						return {
+							exe = "npx prettier --write .",
+						}
+					end,
 				},
 			},
 			["*"] = {
