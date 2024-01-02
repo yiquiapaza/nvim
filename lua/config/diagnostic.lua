23deaa698fe8cb6be03b40ee4482e006321df32f
Yhonatan Jesus <yiquiapaza@gmail.com>
2024-01-01 21:52:43 -0500

    feat(plugins): Add neo tree and diagnostic icons was changed

--- lua/config/diagnostic.lua
+++ lua/config/diagnostic.lua
@@ -2,7 +2,7 @@ vim.diagnostic.config {
     virtual_text = true,
     underline = true,
 }
-local signs = { Error = "", Warn = "", Hint = "", Info = "", Other = "" }
+local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "", Other = "" }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,93 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencoes = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    keys = {
+        { "<leader>tn", ":Neotree filesystem<CR>", desc = "Open NeoTree node" },
+        { "<leader>cn", ":Neotree close<CR>", desc = "Close NeoTree node" },
+        { "<leader>gn", ":Neotree git_status<CR>", desc = "Open Git status NeoTree" },
+    },
+    config = function()
+        vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+        vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+        vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+        vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+        require("neo-tree").setup {
+            close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            enable_normal_mode_for_inputs = false, -- Enable normal mode for input dialogs.
+            open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+            sort_case_insensitive = false, -- used when sorting files and directories in the tree
+            sort_function = nil, -- use a custom function for sorting files and directories in the tree
+            -- sort_function = function (a,b)
+            --       if a.type == b.type then
+            --           return a.path > b.path
+            --       else
+            --           return a.type > b.type
+            --       end
+            --   end , -- this sorts files and directories descendantly
+            -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+            container = {
+                enable_character_fade = true,
+            },
+            indent = {
+                indent_size = 2,
+                padding = 1, -- extra padding on left hand side
+                -- indent guides
+                with_markers = true,
+                indent_marker = "│",
+                last_indent_marker = "└",
+                highlight = "NeoTreeIndentMarker",
+                -- expander config, needed for nesting files
+                with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander",
+            },
+            default_component_configs = {
+                icon = {
+                    folder_closed = "",
+                    folder_open = "",
+                    folder_empty = "󰜌",
+                    -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+                    -- then these will never be used.
+                    default = "*",
+                    highlight = "NeoTreeFileIcon",
+                },
+                modified = {
+                    symbol = "[+]",
+                    highlight = "NeoTreeModified",
+                },
+                name = {
+                    trailing_slash = false,
+                    use_git_status_colors = true,
+                    highlight = "NeoTreeFileName",
+                },
+                git_status = {
+                    ymbols = {
+                        -- Change type
+                        added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                        deleted = "✖", -- this can only be used in the git_status source
+                        renamed = "󰁕", -- this can only be used in the git_status source
+                        -- Status type
+                        untracked = "",
+                        ignored = "",
+                        unstaged = "󰄱",
+                        staged = "",
+                        conflict = "",
+                    },
+                },
+            },
+            window = {
+                position = "right",
+            },
+        }
+    end,
+}
