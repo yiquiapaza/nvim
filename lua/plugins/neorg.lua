89e6a5c323ac12da5d2aca6e64703ac5981145e4
Yhonatan Jesus <yiquiapaza@gmail.com>
2024-01-13 20:34:49 -0500

    feat: add neorg for notes

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,33 @@
+return {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim"},
+    keys = {
+        {"<leader>nw", ":Neorg workspace work", desc = "Open work notes"},
+        {"<leader>np", ":Neorg workspace personal", desc = "Open personal notes"},
+        {"<leader>nr", ":Neorg workspace research", desc = "Open research notes"},
+        {"<leader>na", ":Neorg workspace academic", desc = "Open academic notes"},
+    },
+    config = function ()
+        require("neorg").setup {
+            load = {
+                ["core.defaults"] = {},
+                ["core.dirman"] = {
+                    config = {
+                        workspaces = {
+                            -- Create dir where you have your notes
+                            -- Personal
+                            personal = "~/Projects/Notes/Personal",
+                            -- Work
+                            work = "~/Projects/Notes/Work",
+                            -- Research
+                            research = "~/Projects/Notes/Research",
+                            -- Academic
+                            academic = "~/Projects/Notes/Academic",
+                        }
+                    }
+                }
+            }
+        }
+    end
+}
