e03ab36d002002fe70d9cc5e6a7b221934bdee79
Yhonatan Jesus <yiquiapaza@gmail.com>
2024-02-27 23:15:09 -0500

    feat: update theme and update treesitter wit lsp

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         require("lualine").setup {
             options = {
-                theme = "catppuccin",
+                theme = "nord",
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
             },
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,27 +1,9 @@
 return {
-    "catppuccin/nvim",
+    "shaunsingh/nord.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
-        require("catppuccin").setup {
-            flavour = "mocha",
-            background = {
-                dark = "mocha",
-            },
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                treesitter = true,
-                noice = false,
-                telescope = {
-                    enabled = true,
-                },
-                harpoon = false,
-                hop = false,
-                lsp_trouble = false,
-            },
-        }
-        vim.cmd.colorscheme "catppuccin"
+        vim.cmd.colorscheme "nord"
     end,
 }
