e3c75f93a476718c3ed3c69789b960758968c3a2
Yhonatan Jesus <yiquiapaza@gmail.com>
2024-07-06 19:16:17 -0500

    feat: add indentation with go and update Trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,7 +3,7 @@ return {
     cmd = { "TroubleToggle", "Trouble" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-        { "<leader>mm", ":Trouble document_diagnostics<CR>", desc = "Trouble Document Diagnostics" },
+        { "<leader>mm", ":Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
         -- vim.api.nvim_set_keymap("n", "<space>mm", ":Trouble document_diagnostics<CR>", { noremap = true })
     },
     config = true,
