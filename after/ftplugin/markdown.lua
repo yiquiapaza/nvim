1cafa7fb6de5752b74ead2353bcd9213b0569619
Yhonatan Jesus <yiquiapaza@gmail.com>
2025-07-01 00:36:50 -0500

    fix: remove rule in markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,7 +2,7 @@
 -- setlocal fo+=aw
 vim.cmd('setlocal wrap')
 vim.cmd('setlocal  linebreak')
-vim.cmd('setlocal colorcolumn=120')
+--vi.cmd('setlocal colorcolumn=120')
 --vim.cmd('setlocal textwidth = 120')
 --vim.opt.textwidth = 120
 --vim.cmd('setlocal list!')
